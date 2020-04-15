51853b9159879789019e3a1c41ac84b6b871852f
kajman <kajmaj87@gmail.com>
2020-04-15 08:07:17 +0200

    Removed stage stats. Looping stages until ` is pressed

--- console_ui.py
+++ console_ui.py
@@ -1,17 +1,19 @@
 from controller import Controller
 from key import Key
 import curses
+import log
 
 
 class Console_ui:
 
-    stage_complete = False
+    should_redraw = False
 
     def write_char(self, key):
         self.stdscr.addch(key.char)
 
-    def close(self):
-        self.stage_complete = True
+    def redraw(self):
+        log.debug("Will redraw screen")
+        self.should_redraw = True
 
     def get_next_key(self):
         return self.stdscr.getkey()
@@ -23,15 +25,23 @@ class Console_ui:
         self.stdscr = stdscr
 
         self.controller = Controller(self)
-        while not self.stage_complete:
-            c = self.get_next_key()
-            if ord(c) == ord(curses.erasechar()):
-                self.controller.sendKey(Key(special="ERASE"))
-                y, x = stdscr.getyx()
-                stdscr.delch(y, x - 1)
-            else:
-                self.controller.sendKey(Key(c))
-        self.stdscr.getkey()
+        c = ""
+        log.debug("Redraw: {}".format(self.should_redraw))
+        while c != "`":  # main loops waits for Ctrl+C
+            while not self.should_redraw and c != "`":
+                c = self.get_next_key()
+                if ord(c) == ord(curses.erasechar()):
+                    self.controller.sendKey(Key(special="ERASE"))
+                    y, x = stdscr.getyx()
+                    stdscr.delch(y, x - 1)
+                else:
+                    self.controller.sendKey(Key(c))
+
+            self.stdscr.getkey()
+            log.debug("Redrawing screen (key: {})".format(c))
+            self.stdscr.clear()
+            self.write(self.controller.start_next_stage())
+            self.should_redraw = False
 
     def loop(self):
         curses.wrapper(self.inner_loop)
--- controller.py
+++ controller.py
@@ -1,4 +1,3 @@
-from stage_stats import StageStats
 from key_logger import KeyLogger
 from transition_aggregator import TransitionAggregator
 from generators import FrequencyBasedGenerator
@@ -11,36 +10,34 @@ class Controller:
     current_text = ""
 
     def __init__(self, output):
-        self.text = FrequencyBasedGenerator(
-            focus(create_dict("data/moby-dick.txt"), ";:,./!?-", gain=100)
-        ).generateText(15)
+        self.start_next_stage()
         self.output = output
-        self.stats = StageStats(self.text)
         self.logger = KeyLogger()
         self.aggregator = TransitionAggregator()
         self.output.write(self.text)
+
+    def start_next_stage(self):
+        self.current_text = ""
+        self.text = FrequencyBasedGenerator(
+            focus(create_dict("data/moby-dick.txt"), ";:,./!?-", gain=100)
+        ).generateText(15)
+        return self.text
 
     def sendKey(self, key):
         if key.char:
             self.current_text += key.char
             self.output.write_char(key)
             if self.current_text == self.text[: self.n]:
-                self.stats.registerKey(key, "CORRECT")
                 self.logger.log_key(key, "CORRECT")
                 self.n += 1
             else:
-                self.stats.registerKey(key, "ERROR")
                 self.logger.log_key(key, "ERROR")
         elif key.special == "ERASE":
             self.current_text = self.current_text[:-1]
-            self.stats.registerKey(key, "ERASE")
             self.logger.log_key(key, "ERASE")
 
         if self.current_text == self.text:
-            self.output.close()
+            self.output.redraw()
             save_transitions(self.logger.keys)
-            self.stats.registerStageEnd()
-            self.output.write("\nStage complete.\n")
-            self.output.write(self.stats.summary())
             self.output.write("\nKey stats:\n")
             self.output.write(self.aggregator.summary(load_transitions()))
