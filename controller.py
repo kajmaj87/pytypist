41e336d81bcbfa5fde40cc1a4651c5e288292e3c
kajman <kajmaj87@gmail.com>
2020-04-15 07:15:49 +0200

    Added file handling for transistions

--- controller.py
+++ controller.py
@@ -2,7 +2,8 @@ from stage_stats import StageStats
 from key_logger import KeyLogger
 from transition_aggregator import TransitionAggregator
 from generators import FrequencyBasedGenerator
-from files import create_dict
+from files import create_dict, save_transitions, load_transitions
+from focus import focus
 
 
 class Controller:
@@ -10,13 +11,11 @@ class Controller:
     current_text = ""
 
     def __init__(self, output):
-        text = "This is a very long text. It contains a lot of bla bla bla bla and other nonmeaningful words."
-        text = "test"
         self.text = FrequencyBasedGenerator(
-            create_dict("data/moby-dick.txt")
-        ).generateText(10)
+            focus(create_dict("data/moby-dick.txt"), ";:,./!?-", gain=100)
+        ).generateText(15)
         self.output = output
-        self.stats = StageStats(text)
+        self.stats = StageStats(self.text)
         self.logger = KeyLogger()
         self.aggregator = TransitionAggregator()
         self.output.write(self.text)
@@ -39,9 +38,9 @@ class Controller:
 
         if self.current_text == self.text:
             self.output.close()
+            save_transitions(self.logger.keys)
             self.stats.registerStageEnd()
             self.output.write("\nStage complete.\n")
             self.output.write(self.stats.summary())
             self.output.write("\nKey stats:\n")
-            #            self.output.write(self.logger.summary())
-            self.output.write(self.aggregator.summary(self.logger.transitions()))
+            self.output.write(self.aggregator.summary(load_transitions()))
--- files.py
+++ files.py
@@ -1,5 +1,12 @@
 from collections import defaultdict
+import os
 import log
+import glob
+import json
+from key_logger import Transition
+from datetime import datetime
+
+transition_path = "stats/transitions"
 
 
 def create_dict(path):
@@ -16,3 +23,25 @@ def create_dict(path):
                 d[t] += 1
     log.debug({k: v for k, v in sorted(d.items(), key=lambda item: -item[1])})
     return d
+
+
+def save_transitions(transitions):
+    def prepare_directory(path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def current_timestamp():
+        return str(datetime.now())
+
+    prepare_directory(transition_path)
+    with open(transition_path + "/" + current_timestamp(), "w") as f:
+        json.dump(transitions, f, indent=2)
+
+
+def load_transitions():
+    files = [f for f in glob.glob(transition_path + "/*")]
+    transitions = []
+    for f in files:
+        with open(f) as current:
+            transitions.extend(json.load(current))
+    return [Transition(t[0], t[1], t[2], t[3]) for t in transitions]
